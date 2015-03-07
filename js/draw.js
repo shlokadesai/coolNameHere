5055458ccb6dad5b6afac102ae35e62dfdeef2ec
Youhao Wei <youhaowei@email.arizona.edu>
2015-03-07 01:57:57 -0700

    change color on hover

--- js/draw.js
+++ js/draw.js
@@ -1,5 +1,17 @@
 var map = new Datamap({
     scope: 'world',
     element: document.getElementById('map'),
+    fills: {
+        // default color is the country without data
+        defaultFill: 'rgba(200,200,200,0.5)'
+    },
+    geographyConfig: {
+        highlightFillColor: '#4F75B4', // dark blue
+        popupTemplate: function(geo, data) {
+            console.log(geo);
+            return '<div class="hoverinfo"><strong>'+geo.id + ' : ' + geo.properties.name+'</strong></div>';
+        }
+    }
 });
+
 
