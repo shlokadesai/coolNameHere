7af4409ca56119c061d70c25bf9c9b5e733f9b60
tpatki <tpatki@email.arizona.edu>
2015-03-07 02:49:08 -0700

    Modified csv files with country ID

--- Rscripts/countryNameConvert.R
+++ Rscripts/countryNameConvert.R
@@ -5,5 +5,5 @@ library('countrycode')
 for (i in 1:numfiles) {
   d<-data.frame(read.csv(paste("C:/Users/Tapasya/Desktop/HackArizona/coolNameHere/data/csv/",filenames[i],sep="")))
   d$ID<-countrycode(d$Country,"country.name", "iso3c")
-  write.csv(d,paste("test", i, ".csv", sep=""),quote=FALSE, row.names=FALSE)
+  write.csv(d,filenames[i],quote=FALSE, row.names=FALSE)
 }
