6cb44bba798c1b79d354bd21dbf4d50d80e81ec1
tpatki <tpatki@email.arizona.edu>
2015-03-07 02:42:54 -0700

    Fixed data 2012 to 2014

---
+++ Rscripts/countryNameConvert.R
@@ -0,0 +1,9 @@
+filenames <- list.files(path="C:/Users/Tapasya/Desktop/HackArizona/coolNameHere/data/csv")  
+numfiles <- length(filenames) 
+library('countrycode')
+
+for (i in 1:numfiles) {
+  d<-data.frame(read.csv(paste("C:/Users/Tapasya/Desktop/HackArizona/coolNameHere/data/csv/",filenames[i],sep="")))
+  d$ID<-countrycode(d$Country,"country.name", "iso3c")
+  write.csv(d,paste("test", i, ".csv", sep=""),quote=FALSE, row.names=FALSE)
+}
---
+++ Rscripts/divBy10.R
@@ -0,0 +1,11 @@
+d1<-data.frame(read.csv("C:/Users/Tapasya/Desktop/HackArizona/coolNameHere/data/csv/cpi2012.csv"))
+d2<-data.frame(read.csv("C:/Users/Tapasya/Desktop/HackArizona/coolNameHere/data/csv/cpi2013.csv"))
+d3<-data.frame(read.csv("C:/Users/Tapasya/Desktop/HackArizona/coolNameHere/data/csv/cpi2014.csv"))
+
+d1$CPI<-d1$CPI/10
+d2$CPI<-d2$CPI/10
+d3$CPI<-d3$CPI/10
+
+write.csv(d1,paste("cpi2012.csv", sep=""),quote=FALSE, row.names=FALSE)
+write.csv(d2,paste("cpi2013.csv", sep=""),quote=FALSE, row.names=FALSE)
+write.csv(d3,paste("cpi2014.csv", sep=""),quote=FALSE, row.names=FALSE)
